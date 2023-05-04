9059d3a690b6689fe363a68b58cbffaeb3c468f4
yinghao.xu <yinghao.xu@tongdun.net>
2023-05-04 21:43:09 +0800

    bug fix

--- model/models.py
+++ model/models.py
@@ -7,13 +7,14 @@
 @time: 2023/4/23 11:17
 @desc:
 '''
-import torch
 import torch.nn as nn
-from helper import *
-from model.compgcn_conv_basis import CompGCNConvBasis
-from model.compgcn_conv import CompGCNConv
-from model.rgat_conv import RGATConv
 from torch_geometric.nn.inits import glorot
+
+from helper import *
+from model.compgcn_conv import CompGCNConv
+from model.compgcn_conv_basis import CompGCNConvBasis
+from model.rgat_conv import RGATConv
+
 
 class BaseModel(torch.nn.Module):
     def __init__(self, params):
@@ -28,7 +29,7 @@ class BaseModel(torch.nn.Module):
 
 
 class RGATBase(BaseModel):
-    def __init__(self,edge_index,edge_type,ent_feature,num_rel,params=None):
+    def __init__(self, edge_index, edge_type, ent_feature, num_rel, params=None):
         super(RGATBase, self).__init__(params)
 
         self.edge_index = edge_index
@@ -38,45 +39,46 @@ class RGATBase(BaseModel):
         # 需要加上节点的额外的embedding特
         # id embedding
         self.device = self.edge_index.device
-        self.id_embed = get_param((self.p.num_ent,self.p.init_dim))
-        self.gender_embed = nn.Embedding(3,self.p.init_dim)
-        self.age_embed = nn.Embedding(9,self.p.init_dim)
-        self.level_embed = nn.Embedding(11,self.p.init_dim)
+        self.id_embed = get_param((self.p.num_ent, self.p.init_dim))
+        self.gender_embed = nn.Embedding(3, self.p.init_dim)
+        self.age_embed = nn.Embedding(9, self.p.init_dim)
+        self.level_embed = nn.Embedding(11, self.p.init_dim)
         # [num_ent,init_dim*4]
         # self.init_embed = torch.concat([self.id_embed,self.gender_embed,self.age_embed,self.level_embed],dim=0)
-        self.init_rel = get_param((num_rel*2, self.p.init_dim*4))
+        self.init_rel = get_param((num_rel * 2, self.p.init_dim * 4))
 
-        self.conv1 = RGATConv(self.p.init_dim*4,self.p.gcn_dim,num_rel,self.p.k_kernel)
-        self.conv2 = RGATConv(self.p.gcn_dim,self.p.embed_dim,num_rel,self.p.k_kernel) if self.p.gcn_layer==2 else None
+        self.conv1 = RGATConv(self.p.init_dim * 4, self.p.gcn_dim, num_rel, self.p.k_kernel)
+        self.conv2 = RGATConv(self.p.gcn_dim, self.p.embed_dim, num_rel,
+                              self.p.k_kernel) if self.p.gcn_layer == 2 else None
 
-    def forward_base(self,sub,rel,drop1,drop2):
+    def forward_base(self, sub, rel, drop1, drop2):
         r = self.init_rel
         # x [N_ent,k,output_channel//k]
-        self.x1 = self.gender_embed(self.ent_feature[:,0].view(-1))
-        self.x2 = self.age_embed(self.ent_feature[:,1].view(-1))
-        self.x3 = self.level_embed(self.ent_feature[:,2].view(-1))
-        self.init_embed = torch.cat([self.id_embed,self.x1,self.x2,self.x3],dim=1).to(self.device)
-        x,r  = self.conv1(self.init_embed,self.edge_index,self.edge_type,rel_emb=r)
+        self.x1 = self.gender_embed(self.ent_feature[:, 0].view(-1))
+        self.x2 = self.age_embed(self.ent_feature[:, 1].view(-1))
+        self.x3 = self.level_embed(self.ent_feature[:, 2].view(-1))
+        self.init_embed = torch.cat([self.id_embed, self.x1, self.x2, self.x3], dim=1).to(self.device)
+        x, r = self.conv1(self.init_embed, self.edge_index, self.edge_type, rel_emb=r)
         x = drop1(x)
-        x,r = self.conv2(x,self.edge_index,self.edge_type,rel_emb=r) if self.p.gcn_layer == 2 else (x, r)
-        x = drop2(x) if self.p.gcn_layer==2 else x
+        x, r = self.conv2(x, self.edge_index, self.edge_type, rel_emb=r) if self.p.gcn_layer == 2 else (x, r)
+        x = drop2(x) if self.p.gcn_layer == 2 else x
         # [batch_ent,k,output_channel//k]
-        sub_emb = torch.index_select(x,0,sub)
+        sub_emb = torch.index_select(x, 0, sub)
         # [batch_ent,output_channel]
-        rel_emb = torch.index_select(r,0,rel)
-        return sub_emb,rel_emb,x
+        rel_emb = torch.index_select(r, 0, rel)
+        return sub_emb, rel_emb, x
 
 
 class RGAT_LINK(RGATBase):
-    def __init__(self,edge_index,edge_type,ent_feature,params=None):
-        super(RGAT_LINK, self).__init__(edge_index,edge_type,ent_feature,params.num_rel,params)
+    def __init__(self, edge_index, edge_type, ent_feature, params=None):
+        super(RGAT_LINK, self).__init__(edge_index, edge_type, ent_feature, params.num_rel, params)
         self.drop = torch.nn.Dropout(self.p.hid_drop)
         self.p = params
 
-        self.w1 = nn.Parameter(torch.Tensor(self.p.embed_dim//self.p.k_kernel,self.p.d_q))
-        self.w2 = nn.Parameter(torch.Tensor(self.p.embed_dim,self.p.d_q))
-        self.w3 = nn.Parameter(torch.Tensor(self.p.embed_dim+self.p.embed_dim//self.p.k_kernel,self.p.d_q))
-        self.lin = nn.Linear(self.p.k_kernel*self.p.d_q,self.p.embed_dim)
+        self.w1 = nn.Parameter(torch.Tensor(self.p.embed_dim // self.p.k_kernel, self.p.d_q))
+        self.w2 = nn.Parameter(torch.Tensor(self.p.embed_dim, self.p.d_q))
+        self.w3 = nn.Parameter(torch.Tensor(self.p.embed_dim + self.p.embed_dim // self.p.k_kernel, self.p.d_q))
+        self.lin = nn.Linear(self.p.k_kernel * self.p.d_q, self.p.embed_dim)
 
         self.reset_paramters()
 
@@ -85,57 +87,64 @@ class RGAT_LINK(RGATBase):
         glorot(self.w2)
         glorot(self.w3)
 
-    def forward(self,sub,rel,obj=None):
+    def forward(self, sub, rel, obj=None):
         sub_emb, rel_emb, all_ent = self.forward_base(sub, rel, self.drop, self.drop)
-        sub_emb = sub_emb.view(-1,self.p.k_kernel,self.p.embed_dim//self.p.k_kernel)
+        sub_emb = sub_emb.view(-1, self.p.k_kernel, self.p.embed_dim // self.p.k_kernel)
         # [batch_ent,output_channel]
-        rel_emb = rel_emb.unsqueeze(dim=1).repeat(1,self.p.k_kernel,1)
+        rel_emb = rel_emb.unsqueeze(dim=1).repeat(1, self.p.k_kernel, 1)
         # [batch_ent,k,d_q]
-        key = torch.matmul(sub_emb,self.w1)
+        key = torch.matmul(sub_emb, self.w1)
         # [batch_ent,k,d_q]
-        query = torch.matmul(rel_emb,self.w2)
+        query = torch.matmul(rel_emb, self.w2)
         # [batch_ent,k]
-        attn = torch.softmax((key*query).sum(dim=-1)/torch.sqrt(torch.tensor(self.p.d_q)),dim=1)
+        attn = torch.softmax((key * query).sum(dim=-1) / torch.sqrt(torch.tensor(self.p.d_q)), dim=1)
         # [batch_ent,k,d_]
-        embe_concat = torch.concat([sub_emb,rel_emb],dim=-1)
+        embe_concat = torch.concat([sub_emb, rel_emb], dim=-1)
         # [batch_ent,k,d_q]
-        embe_concat = torch.matmul(embe_concat,self.w3)*(attn.view(-1,self.p.k_kernel,1))
+        embe_concat = torch.matmul(embe_concat, self.w3) * (attn.view(-1, self.p.k_kernel, 1))
         # [batch_ent,k*d_q]
-        embe_concat = embe_concat.view(-1,self.p.k_kernel*self.p.d_q)
+        embe_concat = embe_concat.view(-1, self.p.k_kernel * self.p.d_q)
         output = torch.tanh(self.lin(embe_concat))
 
         if obj is None:
             # [batch_size,num_ent]
             x = torch.mm(output, all_ent.transpose(1, 0))
         else:
-            dst_emb = torch.index_select(all_ent,0,obj)
+            dst_emb = torch.index_select(all_ent, 0, obj)
             # [batch_size]
-            x = torch.sum(output*dst_emb,dim=1,keepdim=False)
+            x = torch.sum(output * dst_emb, dim=1, keepdim=False)
         score = torch.sigmoid(x)
         return score
 
+
 class CompGCNBase(BaseModel):
-    def __init__(self, edge_index, edge_type, num_rel, params=None):
+    def __init__(self, edge_index, edge_type,ent_feature, num_rel, params=None):
         super(CompGCNBase, self).__init__(params)
 
         self.edge_index = edge_index
         self.edge_type = edge_type
+        self.ent_feature = ent_feature
         self.p.gcn_dim = self.p.embed_dim if self.p.gcn_layer == 1 else self.p.gcn_dim
-        self.init_embed = get_param((self.p.num_ent,self.p.init_dim))
+        # self.init_embed = get_param((self.p.num_ent, self.p.init_dim))
         self.device = self.edge_index.device
 
+        self.id_embed = get_param((self.p.num_ent, self.p.init_dim))
+        self.gender_embed = nn.Embedding(3, self.p.init_dim)
+        self.age_embed = nn.Embedding(9, self.p.init_dim)
+        self.level_embed = nn.Embedding(11, self.p.init_dim)
+
         if self.p.num_bases > 0:
-            self.init_rel  = get_param((self.p.num_bases, self.p.init_dim))
+            self.init_rel = get_param((self.p.num_bases, self.p.init_dim*4))
 
         else:
             if self.p.score_func == 'transe':
-                self.init_rel = get_param((num_rel, self.p.init_dim))
+                self.init_rel = get_param((num_rel, self.p.init_dim*4))
             else:
-                self.init_rel = get_param((num_rel*2, self.p.init_dim))
+                self.init_rel = get_param((num_rel * 2, self.p.init_dim*4))
 
         if self.p.num_bases > 0:
             self.conv1 = CompGCNConvBasis(self.p.init_dim, self.p.gcn_dim, num_rel, self.p.num_bases, act=self.act,
-                                         cache=self.p.cache, params=self.p)
+                                          cache=self.p.cache, params=self.p)
             self.conv2 = CompGCNConv(self.p.gcn_dim, self.p.embed_dim, num_rel, act=self.act,
                                      params=self.p) if self.p.gcn_layer == 2 else None
         else:
@@ -147,6 +156,10 @@ class CompGCNBase(BaseModel):
 
     def forward_base(self, sub, rel, drop1, drop2):
         r = self.init_rel if self.p.score_func != 'transe' else torch.cat([self.init_rel, -self.init_rel], dim=0)
+        self.x1 = self.gender_embed(self.ent_feature[:, 0].view(-1))
+        self.x2 = self.age_embed(self.ent_feature[:, 1].view(-1))
+        self.x3 = self.level_embed(self.ent_feature[:, 2].view(-1))
+        self.init_embed = torch.cat([self.id_embed, self.x1, self.x2, self.x3], dim=1).to(self.device)
         x, r = self.conv1(self.init_embed, self.edge_index, self.edge_type, rel_embed=r)
         x = drop1(x)
         x, r = self.conv2(x, self.edge_index, self.edge_type, rel_embed=r) if self.p.gcn_layer == 2 else (x, r)
@@ -163,7 +176,7 @@ class CompGCN_TransE(CompGCNBase):
         super(self.__class__, self).__init__(edge_index, edge_type, params.num_rel, params)
         self.drop = torch.nn.Dropout(self.p.hid_drop)
 
-    def forward(self, sub, rel,obj=None):
+    def forward(self, sub, rel, obj=None):
         sub_emb, rel_emb, all_ent = self.forward_base(sub, rel, self.drop, self.drop)
         obj_emb = sub_emb + rel_emb
 
@@ -178,7 +191,7 @@ class CompGCN_DistMult(CompGCNBase):
         super(self.__class__, self).__init__(edge_index, edge_type, params.num_rel, params)
         self.drop = torch.nn.Dropout(self.p.hid_drop)
 
-    def forward(self, sub, rel,obj=None):
+    def forward(self, sub, rel, obj=None):
         """
 
         Parameters
@@ -199,9 +212,9 @@ class CompGCN_DistMult(CompGCNBase):
             # [batch_size,num_ent]
             x = torch.mm(obj_emb, all_ent.transpose(1, 0))
         else:
-            dst_emb = torch.index_select(all_ent,0,obj)
+            dst_emb = torch.index_select(all_ent, 0, obj)
             # [batch_size]
-            x = torch.sum(obj_emb*dst_emb,dim=1,keepdim=False)
+            x = torch.sum(obj_emb * dst_emb, dim=1, keepdim=False)
         # x += self.bias.expand_as(x)
         score = torch.sigmoid(x)
         return score
@@ -233,7 +246,7 @@ class CompGCN_ConvE(CompGCNBase):
         stack_inp = torch.transpose(stack_inp, 2, 1).reshape((-1, 1, 2 * self.p.k_w, self.p.k_h))
         return stack_inp
 
-    def forward(self, sub, rel,obj=None):
+    def forward(self, sub, rel, obj=None):
         sub_emb, rel_emb, all_ent = self.forward_base(sub, rel, self.hidden_drop, self.feature_drop)
         stk_inp = self.concat(sub_emb, rel_emb)
         x = self.bn0(stk_inp)
@@ -250,8 +263,8 @@ class CompGCN_ConvE(CompGCNBase):
         if obj is None:
             x = torch.mm(x, all_ent.transpose(1, 0))
         else:
-            dst_emb = torch.index_select(all_ent,0,obj)
-            x = torch.sum(x*dst_emb,dim=1,keepdim=False)
+            dst_emb = torch.index_select(all_ent, 0, obj)
+            x = torch.sum(x * dst_emb, dim=1, keepdim=False)
         # x += self.bias.expand_as(x)
         score = torch.sigmoid(x)
         return score
