5e1acb81606b65e10521d524fafd1eb7cbf51e87
yinghao.xu <yinghao.xu@tongdun.net>
2023-05-11 16:28:54 +0800

    bug fix

--- model/models.py
+++ model/models.py
@@ -147,13 +147,14 @@ class RHGATBase(BaseModel):
         # 需要加上节点的额外的embedding特
         # id embedding
         self.device = self.edge_index.device
-        if self.p.feature_method == 'concat':
-            self.p.init_dim = self.p.init_dim * 4
 
         self.id_embed = get_param((self.p.num_ent, self.p.init_dim))
         self.gender_embed = nn.Embedding(3, self.p.init_dim)
         self.age_embed = nn.Embedding(9, self.p.init_dim)
         self.level_embed = nn.Embedding(11, self.p.init_dim)
+
+        if self.p.feature_method == 'concat':
+            self.p.init_dim = self.init_dim * 4
         self.init_rel = get_param((num_rel * 2, self.p.init_dim))
 
         self.conv1 = RGHATConv(self.p.init_dim,self.p.gcn_dim,heads=self.p.heads,num_rels=num_rel,params=params)
